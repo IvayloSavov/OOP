b8d9aabffdfece792e61ebf5a6905b9a68c369fb
Ivaylo Savov <ivaylo_ss@abv.bg>
2020-11-10 09:43:34 +0200

    Solving 04. problem and creating project for the Encapsulation Exercise

---
+++ Encapsulation/01. Person.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.__age = age
+#
+#     def get_name(self):
+#         return self.__name
+#
+#     def get_age(self):
+#         return self.__age
